a7d38cdf7f332f4efeaa07ff06f7c94ec16f6aac
cogle <thecogle@gmail.com>
2016-08-07 13:18:27 -0500

    Working on scaffold

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,16 @@
 defmodule Foundation.PageController do
   use Foundation.Web, :controller
 
+  def about(conn, _params) do
+    render conn, "about.html"
+  end
+
+  def contact(conn, _params) do
+    render conn, "contact.html"
+  end
+
   def index(conn, _params) do
     render conn, "index.html"
   end
+
 end
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,10 @@ defmodule Foundation.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/about", PageController, :about
+    get "/contact", PageController, :contact
+
+    
   end
 
   # Other scopes may use custom stacks.
